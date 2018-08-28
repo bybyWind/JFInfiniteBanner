afddc21d5627e750b7a448bbbe45c12b770c321e
陈经风 <540804236@qq.com>
2018-08-28 10:17:59 +0800

    Initial Commit

---
+++ JFInfiniteBanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JFInfiniteBanner
+//
+//  Created by 社会人 on 2018/8/28.
+//  Copyright © 2018年 社会人. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
